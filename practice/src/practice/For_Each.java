74fdfbbf6f9ca2faaf6395d80f7a328b715d85fe
raining93 <shinbi8836@gmail.com>
2023-02-28 00:23:00 +0900

    create My_practice repository

---
+++ practice/src/practice/For_Each.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class For_Each {
+
+	public static void main(String[] args) {
+		String[] yumis= {"김유미","유바비","루비"};
+		for(String e : yumis) {
+			System.out.println(e + "가 츄로스를 먹습니다.");
+		}
+		
+		
+	}
+
+}
